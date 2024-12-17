1108ad82e190aea5a1091bff8534440e2b7eed0e
JD-SWaresol <juan.delgado@smartwaresol.com>
2024-12-17 11:51:39 -0600

    07_Dynamic Template Values

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -5,9 +5,21 @@ import { HousingLocation } from '../housinglocation';
   selector: 'app-housing-location',
   imports: [],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">      
+      <img        
+        class="listing-photo"        
+        [src]="housingLocation.photo"        
+        alt="Exterior photo of {{ housingLocation.name }}"        
+        crossorigin      
+      />      
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>      
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>    
+    </section>
+    <!--
+      'src' guarda la propiedad para construir 'housingLocation.photo'.
+      'alt' hace una interpolacion para proporcionar mas contexto al texto del titulo.
+      la interpolacion esta presente para mostrar valores de 'name', 'city' y 'state'
+    -->
   `,
   styleUrls: ['./housing-location.component.css']
 })
