4c6dbdc95485fbdb16e878987b8bb7e6bacba454
JD-SWaresol <juan.delgado@smartwaresol.com>
2024-12-19 10:27:56 -0600

    13_Search

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,17 +9,22 @@ import { HousingService } from '../housing.service';
   imports: [HousingLocationComponent, NgForOf],
   template: `
     <section>      
-      <form>        
-        <input type="text" placeholder="Filter by city" />        
-        <button class="primary" type="button">Search</button>      
+      <form>       
+        <!-- Asignamos el nombre de elemento 'filter' para el campo de texto --> 
+        <input type="text" placeholder="Filter by city" #filter />  
+        
+        <!-- Adjumtamos el evento de consulta al momento de presionar el boton -->
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>      
       </form>    
     </section>
     <section class="results">      
       <!-- 
         Se hace un recorrido de los elementos listados en housingLocationList de HomeComponent
+
+        * Actualizamos la lista recorrida por 'filteredLocationList' ya que será la lista que considerará los filtrados de busqueda
       -->
       <app-housing-location        
-      *ngFor="let housingLocation of housingLocationList"        
+      *ngFor="let housingLocation of filteredLocationList"        
       [housingLocation]="housingLocation">
       </app-housing-location>    
     </section>
@@ -34,7 +39,29 @@ export class HomeComponent {
 
   housingService: HousingService = inject(HousingService);
 
+  // Esto guardara los valores que haran match para el criterio de busqueda ingresado por el usuario.
+  // Debe contener el total de valores de locaciones seteadas por default cuando la pagina carga.
+  filteredLocationList: HousingLocation[] = [];
+
+
   constructor(){
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
+
+  //Metodo para realizar el filtrado de texto para la consulta, recibe el valor del campo de texto de la consulta.
+  filterResults(text: string){
+    if(!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>      
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),    
+    );
+
+    // Esta Funcion usa String filter para comparar el valor de el parametro de texto contra la propiedad 'housingLocation.city'
+  }
+
+  
 }
